a7ebd667a0b3c9f9dad34a762991a4664cb1d38b
GabrielLima2803 <gabriellima2803@gmail.com>
2024-03-23 16:17:13 -0300

    Migrate

--- api-dotnet/Migrations/AppDbContextModelSnapshot.cs
+++ api-dotnet/Migrations/AppDbContextModelSnapshot.cs
@@ -17,6 +17,53 @@ namespace api_dotnet.Migrations
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.3");
 
+            modelBuilder.Entity("api_dotnet.models.Categoria", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("Nome")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Categorias");
+                });
+
+            modelBuilder.Entity("api_dotnet.models.Produto", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("INTEGER");
+
+                    b.Property<int>("CategoriaId")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("Descricao")
+                        .IsRequired()
+                        .HasMaxLength(450)
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("Nome")
+                        .IsRequired()
+                        .HasMaxLength(100)
+                        .HasColumnType("TEXT");
+
+                    b.Property<decimal>("Preco")
+                        .HasColumnType("TEXT");
+
+                    b.Property<int>("Quantidade")
+                        .HasColumnType("INTEGER");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("CategoriaId");
+
+                    b.ToTable("Produtos");
+                });
+
             modelBuilder.Entity("api_dotnet.models.estudantes.Estudante", b =>
                 {
                     b.Property<Guid>("Id")
@@ -33,6 +80,22 @@ namespace api_dotnet.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Estudantes");
+                });
+
+            modelBuilder.Entity("api_dotnet.models.Produto", b =>
+                {
+                    b.HasOne("api_dotnet.models.Categoria", "Categoria")
+                        .WithMany("Produtos")
+                        .HasForeignKey("CategoriaId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Categoria");
+                });
+
+            modelBuilder.Entity("api_dotnet.models.Categoria", b =>
+                {
+                    b.Navigation("Produtos");
                 });
 #pragma warning restore 612, 618
         }
